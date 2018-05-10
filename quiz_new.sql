cbac96612131e3a6a1ecab1ad4ecf047fe4b1d75
Dhairya Chandra <dhairyachandra@outlook.com>
2018-05-10 15:22:04 +0530

    SQL file added

---
+++ quiz_new.sql
@@ -0,0 +1,287 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 10, 2018 at 10:44 AM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 5.6.35
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `quiz_new`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mst_admin`
+--
+
+CREATE TABLE `mst_admin` (
+  `id` int(11) NOT NULL,
+  `loginid` varchar(50) NOT NULL,
+  `pass` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mst_admin`
+--
+
+INSERT INTO `mst_admin` (`id`, `loginid`, `pass`) VALUES
+(1, 'admin', 'admin@123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mst_question`
+--
+
+CREATE TABLE `mst_question` (
+  `que_id` int(5) NOT NULL,
+  `test_id` int(5) DEFAULT NULL,
+  `que_desc` varchar(150) DEFAULT NULL,
+  `ans1` varchar(75) DEFAULT NULL,
+  `ans2` varchar(75) DEFAULT NULL,
+  `ans3` varchar(75) DEFAULT NULL,
+  `ans4` varchar(75) DEFAULT NULL,
+  `true_ans` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mst_question`
+--
+
+INSERT INTO `mst_question` (`que_id`, `test_id`, `que_desc`, `ans1`, `ans2`, `ans3`, `ans4`, `true_ans`) VALUES
+(41, 12, 'What is HTML', 'HTML', '5', 'hg', 'uyg', 1),
+(42, 12, 'How to save HTML file ?', '.txt', '.java', '.html', '.css', 3),
+(43, 13, 'what is ?', 'a', 'b', 'c', 'd', 3),
+(44, 14, 'vsvdsvdhv ?', 'a', 'd', 'c', 'g', 2),
+(45, 14, 'udhsjdhsgdjgsd?', '4', 'w', '7', '5', 2),
+(46, 15, 'rthyjghkl', 'q', 'd', 'c', 'b', 2),
+(47, 15, 'hfgjhbfjhsbf?', 't ', 'f', 'k', '8', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mst_result`
+--
+
+CREATE TABLE `mst_result` (
+  `login` varchar(20) DEFAULT NULL,
+  `test_id` int(5) DEFAULT NULL,
+  `test_date` date DEFAULT NULL,
+  `score` int(3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mst_result`
+--
+
+INSERT INTO `mst_result` (`login`, `test_id`, `test_date`, `score`) VALUES
+('raj', 8, '0000-00-00', 3),
+('raj', 9, '0000-00-00', 3),
+('raj', 8, '0000-00-00', 1),
+('ashish', 10, '0000-00-00', 3),
+('ashish', 9, '0000-00-00', 2),
+('ashish', 10, '0000-00-00', 0),
+('raj', 8, '0000-00-00', 0),
+('ankur', 11, '0000-00-00', 0),
+('dhairya', 12, '0000-00-00', 2),
+('test', 12, '0000-00-00', 1),
+('test', 13, '0000-00-00', 0),
+('dhairya', 14, '0000-00-00', 2),
+('dhairya', 12, '0000-00-00', 1),
+('dhairya', 12, '0000-00-00', 1),
+('dhairya', 15, '0000-00-00', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mst_subject`
+--
+
+CREATE TABLE `mst_subject` (
+  `sub_id` int(5) NOT NULL,
+  `sub_name` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mst_subject`
+--
+
+INSERT INTO `mst_subject` (`sub_id`, `sub_name`) VALUES
+(10, 'Web'),
+(11, 'JAVA'),
+(12, 'OS'),
+(13, 'sub1'),
+(14, 'TEST123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mst_test`
+--
+
+CREATE TABLE `mst_test` (
+  `test_id` int(5) NOT NULL,
+  `sub_id` int(5) DEFAULT NULL,
+  `test_name` varchar(30) DEFAULT NULL,
+  `total_que` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mst_test`
+--
+
+INSERT INTO `mst_test` (`test_id`, `sub_id`, `test_name`, `total_que`) VALUES
+(12, 10, 'Prac Exam', '2'),
+(13, 11, 'PRAC', '3'),
+(14, 12, 'MId Sem', '2'),
+(15, 14, 'final exam', '2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mst_user`
+--
+
+CREATE TABLE `mst_user` (
+  `user_id` int(5) NOT NULL,
+  `login` varchar(20) DEFAULT NULL,
+  `pass` varchar(20) DEFAULT NULL,
+  `username` varchar(30) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `city` varchar(15) DEFAULT NULL,
+  `phone` int(10) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mst_user`
+--
+
+INSERT INTO `mst_user` (`user_id`, `login`, `pass`, `username`, `address`, `city`, `phone`, `email`) VALUES
+(1000, 'dhairya', 'dhairya', 'dhairya', '123', 'chd', 989898989, 'hg@jh.dd'),
+(1001, 'test', '123', 'tester', '', '', 0, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mst_useranswer`
+--
+
+CREATE TABLE `mst_useranswer` (
+  `sess_id` varchar(80) DEFAULT NULL,
+  `test_id` int(11) DEFAULT NULL,
+  `que_des` varchar(200) DEFAULT NULL,
+  `ans1` varchar(50) DEFAULT NULL,
+  `ans2` varchar(50) DEFAULT NULL,
+  `ans3` varchar(50) DEFAULT NULL,
+  `ans4` varchar(50) DEFAULT NULL,
+  `true_ans` int(11) DEFAULT NULL,
+  `your_ans` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mst_useranswer`
+--
+
+INSERT INTO `mst_useranswer` (`sess_id`, `test_id`, `que_des`, `ans1`, `ans2`, `ans3`, `ans4`, `true_ans`, `your_ans`) VALUES
+('2b8e3337837b82112def8d3e2f42f26e', 8, 'What  Default Data Type ?', 'String', 'Variant', 'Integer', 'Boolear', 2, 1),
+('2b8e3337837b82112def8d3e2f42f26e', 8, 'What is Default Form Border Style ?', 'Fixed Single', 'None', 'Sizeable', 'Fixed Diaglog', 3, 3),
+('2b8e3337837b82112def8d3e2f42f26e', 8, 'Which is not type of Control ?', 'text', 'lable', 'checkbox', 'option button', 1, 3),
+('idjir9pcq2d07764us8rdiq9n5', 11, 'how to use date( ) in mysql ?', 'now( )', 'today( )', 'date( )', 'time( )', 0, 1),
+('idjir9pcq2d07764us8rdiq9n5', 11, 'how to use date( ) in mysql ?', 'now( )', 'today( )', 'date( )', 'time( )', 0, 1),
+('idjir9pcq2d07764us8rdiq9n5', 11, 'how to use date( ) in mysql ?', 'now( )', 'today( )', 'date( )', 'time( )', 0, 2),
+('idjir9pcq2d07764us8rdiq9n5', 11, 'how to use date( ) in mysql ?', 'now( )', 'today( )', 'date( )', 'time( )', 0, 3),
+('idjir9pcq2d07764us8rdiq9n5', 11, 'how to use date( ) in mysql ?', 'now( )', 'today( )', 'date( )', 'time( )', 0, 4),
+('idjir9pcq2d07764us8rdiq9n5', 11, 'how to use date( ) in mysql ?', 'now( )', 'today( )', 'date( )', 'time( )', 0, 4),
+('idjir9pcq2d07764us8rdiq9n5', 11, 'how to use date( ) in mysql ?', 'now( )', 'today( )', 'date( )', 'time( )', 0, 3),
+('idjir9pcq2d07764us8rdiq9n5', 11, 'how to use date( ) in mysql ?', 'now( )', 'today( )', 'date( )', 'time( )', 0, 2),
+('idjir9pcq2d07764us8rdiq9n5', 11, 'how to use date( ) in mysql ?', 'now( )', 'today( )', 'date( )', 'time( )', 0, 2),
+('idjir9pcq2d07764us8rdiq9n5', 11, 'how to use date( ) in mysql ?', 'now( )', 'today( )', 'date( )', 'time( )', 0, 1),
+('idjir9pcq2d07764us8rdiq9n5', 11, 'how to use date( ) in mysql ?', 'now( )', 'today( )', 'date( )', 'time( )', 0, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mst_admin`
+--
+ALTER TABLE `mst_admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mst_question`
+--
+ALTER TABLE `mst_question`
+  ADD PRIMARY KEY (`que_id`);
+
+--
+-- Indexes for table `mst_subject`
+--
+ALTER TABLE `mst_subject`
+  ADD PRIMARY KEY (`sub_id`);
+
+--
+-- Indexes for table `mst_test`
+--
+ALTER TABLE `mst_test`
+  ADD PRIMARY KEY (`test_id`);
+
+--
+-- Indexes for table `mst_user`
+--
+ALTER TABLE `mst_user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mst_admin`
+--
+ALTER TABLE `mst_admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `mst_question`
+--
+ALTER TABLE `mst_question`
+  MODIFY `que_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+
+--
+-- AUTO_INCREMENT for table `mst_subject`
+--
+ALTER TABLE `mst_subject`
+  MODIFY `sub_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `mst_test`
+--
+ALTER TABLE `mst_test`
+  MODIFY `test_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `mst_user`
+--
+ALTER TABLE `mst_user`
+  MODIFY `user_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1002;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
